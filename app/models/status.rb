5820562349aa6e1dbf4781c63549636e534aab23
DonCox <tvillcox@bellsouth.net>
2014-04-24 11:55:48 -0400

    Update frontend for the new authentication system
    Added simple_form
    Updated several forms to bootstrap/simple_form
    Added login/logout/register/profile links to our nav
    Added a collection select to our status form

--- app/models/status.rb
+++ app/models/status.rb
@@ -1,3 +1,8 @@
 class Status < ActiveRecord::Base
-  
+  #attr_accessible :content, :user
+  belongs_to :user
+end
+
+class User < ActiveRecord::Base
+  has_many :statuses
 end
--- app/models/user.rb
+++ app/models/user.rb
@@ -6,4 +6,5 @@ class User < ActiveRecord::Base
          
   #attr_accessible :email, :password, :password_confirmation, :remember_me,
   #                :first_name, :last_name, :profile_name
+  
 end
