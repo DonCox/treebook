abe4796e98c7f5d14b851caeaf1d6c0e32c8a093
DonCox <tvillcox@bellsouth.net>
2014-04-30 19:10:14 -0400

    Added gravatar

--- app/models/user.rb
+++ app/models/user.rb
@@ -24,4 +24,12 @@ class User < ActiveRecord::Base
     first_name + " " + last_name
   end
   
+  def gravatar_url
+    stripped_email = email.strip
+    downcased_email = stripped_email.downcase
+    hash = Digest::MD5.hexdigest(downcased_email)
+    # will not work unless you have signed up for avatar
+    "http://gravatar.com/avatar/#{hash}"
+  end
+  
 end
